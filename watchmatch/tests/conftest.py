9fdfdcafe9c8237604fe89559ec5183d2ebee658
Roman Kremeshniy <sokems@mail.ru>
2026-02-21 16:08:43 +0500

    Update tests, and improve terminal output

--- watchmatch/tests/conftest.py
+++ watchmatch/tests/conftest.py
@@ -9,14 +9,10 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 @pytest.fixture
-def url_admin_index():
-    """Возвращает URL страницы админки."""
-    return reverse('admin:index')
-
-@pytest.fixture
 def auth_user(django_user_model):
     """Создает зарегистрированного пользователя."""
     return django_user_model.objects.create(username='sokem')
+
 
 @pytest.fixture
 def auth_user_client(auth_user):
@@ -25,3 +21,16 @@ def auth_user_client(auth_user):
     client.force_login(auth_user)
 
     return client
+
+
+@pytest.fixture
+def url_admin_index():
+    """Возвращает URL страницы админки."""
+    return reverse('admin:index')
+
+
+@pytest.fixture
+def url_homepage_index():
+    """Возвращает URL главной страницы"""
+    return reverse('core:index')
+
--- watchmatch/tests/test_routers.py
+++ watchmatch/tests/test_routers.py
@@ -16,7 +16,8 @@ def test_admin_panel_access_for_superuser(admin_client, url_admin_index):
 
     response = admin_client.get(url_admin_index)
 
-    print(f'\n{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
+    print(f'\n{Fore.BLUE}request: {Fore.MAGENTA}{response.request}{Fore.RESET}')
+    print(f'{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
     assert response.status_code == HTTPStatus.OK
 
 
@@ -28,7 +29,8 @@ def test_admin_panel_access_for_anonymous_user(client, url_admin_index):
     """
     response = client.get(url_admin_index)
 
-    print(f'\n{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
+    print(f'\n{Fore.BLUE}request: {Fore.MAGENTA}{response.request}{Fore.RESET}')
+    print(f'{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
     assert response.status_code == HTTPStatus.FOUND
     assert "/admin/login/" in response.url
 
@@ -40,7 +42,17 @@ def test_admin_panel_access_for_authenticated_user(auth_user_client, url_admin_i
     не может попасть на страницу администратора.
     """
     response = auth_user_client.get(url_admin_index)
-
-    print(f'\n{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
+    print(f'\n{Fore.BLUE}request: {Fore.MAGENTA}{response.request}{Fore.RESET}')
+    print(f'{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
     assert response.status_code == HTTPStatus.FOUND
     assert "/admin/login/" in response.url
+
+
+def test_homepage_is_available(client, url_homepage_index):
+    """
+    Проверяет доступность главной страницы для любого пользователя
+    """
+    response = client.get(url_homepage_index)
+    print(f'\n{Fore.BLUE}request: {Fore.MAGENTA}{response.request}{Fore.RESET}')
+    print(f'{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
+    assert response.status_code == HTTPStatus.OK
