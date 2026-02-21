d76ae145814c1faffbe7723d30885800e43e8abf
Roman Kremeshniy <sokems@mail.ru>
2026-02-21 15:52:18 +0500

    Integrate colorama, update tests, and improve terminal output

--- watchmatch/tests/conftest.py
+++ watchmatch/tests/conftest.py
@@ -9,24 +9,6 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 @pytest.fixture
-def superuser(django_user_model):
-    """Создает и возвращает супер юзера."""
-    return django_user_model.objects.create_superuser(
-        'admin',
-        'admin@test.com',
-        'password123'
-    )
-
-
-@pytest.fixture
-def superuser_client(superuser):
-    """Возвращает клиент Django с авторизованным супер юзером."""
-    client = Client()
-    client.force_login(superuser)
-    return client
-
-
-@pytest.fixture
 def url_admin_index():
     """Возвращает URL страницы админки."""
     return reverse('admin:index')
---
+++ watchmatch/tests/test_routers.py
@@ -0,0 +1,46 @@
+from http import HTTPStatus
+
+import pytest
+from colorama import init, Fore
+
+
+init(autoreset=True)
+
+
+@pytest.mark.django_db
+#@pytest.mark.xfail(reason='Пусть пока падает')
+def test_admin_panel_access_for_superuser(admin_client, url_admin_index):
+    """
+    Проверяет, что суперпользователь имеет доступ к панели администратора.
+    """
+
+    response = admin_client.get(url_admin_index)
+
+    print(f'\n{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
+    assert response.status_code == HTTPStatus.OK
+
+
+@pytest.mark.django_db
+def test_admin_panel_access_for_anonymous_user(client, url_admin_index):
+    """
+    Проверяет, что неавторизованный пользователь
+    не может попасть на страницу администратора.
+    """
+    response = client.get(url_admin_index)
+
+    print(f'\n{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
+    assert response.status_code == HTTPStatus.FOUND
+    assert "/admin/login/" in response.url
+
+
+@pytest.mark.django_db
+def test_admin_panel_access_for_authenticated_user(auth_user_client, url_admin_index):
+    """
+    Проверяет, что авторизованный пользователь
+    не может попасть на страницу администратора.
+    """
+    response = auth_user_client.get(url_admin_index)
+
+    print(f'\n{Fore.BLUE}response: {Fore.MAGENTA}{response}{Fore.RESET}\n')
+    assert response.status_code == HTTPStatus.FOUND
+    assert "/admin/login/" in response.url
