18975d7070e86414c5cd32be45b166865e18d9de
Roman Kremeshniy <sokems@mail.ru>
2026-02-22 13:19:27 +0500

    Create base HTML templates for room creation, connection, game, and movie pages

--- watchmatch/movies/views.py
+++ watchmatch/movies/views.py
@@ -1,6 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def detail_movie(request, movie_id):
-    """Описание фильма"""
-    return HttpResponse(f'Фильм с id: {movie_id}')
+    """Страница фильма"""
+    template_name = 'movies/detail_movie.html'
+    return render(request, template_name)
--- watchmatch/rooms/views.py
+++ watchmatch/rooms/views.py
@@ -1,16 +1,19 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def create_room(request):
     """Создание новой комнаты для игры"""
-    return HttpResponse('Создание комнаты')
+    template_name = 'rooms/create_room.html'
+    return render(request, template_name)
 
 
 def play_room(request, room_id):
     """Комната для игры"""
-    return HttpResponse(f'Комната с id: {room_id}')
+    template_name = 'rooms/play_room.html'
+    return render(request, template_name)
 
 
 def join_room(request):
     """Страница подключения к комнате"""
-    return HttpResponse('Введите id комнаты')
+    template_name = 'rooms/join_room.html'
+    return render(request, template_name)
