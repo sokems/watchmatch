270a150f95701f01a85bc620c645fdf4b541f22b
Roman Kremeshniy <sokems@mail.ru>
2026-02-23 12:08:27 +0500

    Override Movie and Genre model IDs and configure admin panel

--- watchmatch/rooms/models.py
+++ watchmatch/rooms/models.py
@@ -15,17 +15,38 @@ class Room(models.Model):
     После создания комнаты участники голосуют за фильмы (механика swipe).
     """
 
-    name = models.CharField(max_length=100, verbose_name='Название')
-    count_participants = models.PositiveSmallIntegerField(verbose_name='Количество участников')
+    name = models.CharField(
+        max_length=100,
+        verbose_name='Название',
+        help_text='Название комнаты'
+    )
+    count_participants = models.PositiveSmallIntegerField(
+        verbose_name='Количество участников'
+    )
     genres = models.ManyToManyField(
         'movies.Genre',
         related_name='rooms',
         verbose_name='Жанры'
     )
-    year_start = models.PositiveSmallIntegerField(verbose_name='Релиз от')
-    year_end = models.PositiveSmallIntegerField(verbose_name='Релиз до')
-    adult = models.BooleanField(verbose_name='18+')
-    vote_average = models.DecimalField(max_digits=4, decimal_places=2, verbose_name='Средний рейтинг')
+    year_start = models.PositiveSmallIntegerField(
+        verbose_name='Релиз от',
+        help_text='Укажите начальный год релиза. Если нужен только один год, '
+                  'укажите одинаковые значения в полях "Релиз от" и "Релиз до"'
+    )
+    year_end = models.PositiveSmallIntegerField(
+        verbose_name='Релиз до',
+        help_text='Укажите конечный год релиза'
+    )
+    adult = models.BooleanField(
+        verbose_name='18+',
+        help_text='Контент с рейтингом 18+'
+    )
+    vote_average = models.DecimalField(
+        max_digits=4,
+        decimal_places=2,
+        verbose_name='Средний рейтинг',
+        help_text='Средний рейтинг по TMDB'
+    )
 
     class Meta:
         verbose_name = 'комната'
